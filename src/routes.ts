058813ab6fc0e447f2aca26ae8360e5f9f5b28c3
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-05-10 12:34:54 -0300

    recomeçando 1.0

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import UsersController from './controllers/User-controller';
-
-const userController = new UsersController
-const routes = Router();
-
-routes.get("/user", userController.view )
-routes.post("/user", userController.create)
-export {routes}
