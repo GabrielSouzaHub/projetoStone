8703a2c3a273c7c9ebebd897e1ff8190b8ed7653
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-05-23 21:50:11 -0300

    Configura jest, para testes

--- dist/index.spec.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('./server');
-describe('Test my app server', () => {
-    it('should get main route', async () => {
-        const res = await request(app)
-            .get('/users');
-        expect(res.body).toHaveProperty('error');
-    });
-});
